28fcba699a59934e70866fece216948a510823ae
Bruno Aguiar Daguis <bruno.daguis@gmail.com>
2019-11-14 11:46:16 -0300

    init

---
+++ PaymentService/Controllers/PaymentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public IEnumerable<string> Get()
+        {
+            return new List<string>
+            {
+                "Débito",
+                "Crédito",
+                "Carne do Bau"
+            };
+        }
+    }
+}
---
+++ ProductService/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public IEnumerable<string> Get()
+        {
+            return new List<string>
+            {
+                "Produto 1",
+                "Produto 2",
+                "Produto 2"
+            };
+        }
+    }
+}
